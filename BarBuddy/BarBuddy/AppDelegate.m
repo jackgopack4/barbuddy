77d870b3c2295eaf2b9abc8f3f49b426e044d087
John L. Peterson <jackgopack4@gmail.com>
2014-05-07 22:31:38 -0500

    added Facebook error checking and functionality

--- BarBuddy/BarBuddy/AppDelegate.m
+++ BarBuddy/BarBuddy/AppDelegate.m
@@ -39,6 +39,7 @@
         [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
         shadow, NSShadowAttributeName,[UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], NSFontAttributeName, nil]];
     [FBLoginView class];
+    [FBProfilePictureView class];
     return YES;
 
 }
